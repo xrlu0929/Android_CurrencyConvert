a5f79c261202e02c38a94f31f012cb411d54042c
xrlu0929 <xl2474@tc.columbia.edu>
2018-07-08 17:02:49 -0400

    init

---
+++ app/src/main/java/com/example/annaxlu/currencyconvert/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.annaxlu.currencyconvert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void convert (View view) {
+        TextView input = findViewById()
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
